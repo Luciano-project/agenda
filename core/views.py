d7ca8056accef24089a6f6b0b1bc5ebcd21314eb
Luciano-project <luhugo.lhcs@gmail.com>
2022-02-17 00:10:30 +0000

    Incluido template de inserção de dados de eventos da agenda.

--- core/views.py
+++ core/views.py
@@ -52,10 +52,24 @@ def lista_eventos(request):
     dados= {'eventos':evento}
     return render(request,'agenda.html', dados)
 
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request,'evento.html')
 
-
-
-
+# Usando a verificação de login
+@login_required(login_url='/login/')
+# aqui vamos buscar os dados que foram inseridos para mandá-los ao banco de dados
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                             data_evento=data_evento,
+                             descricao=descricao,
+                             usuario=usuario)
+    return redirect('/')
 # Classe para exibir o nome de determinado evento:
 #def evento_django(request,none):
     #evedj=Evento.titulo
